2e30310a43772c9eb3cbab271cfe30bdacb41f81
dilinsh <dilinsh@gmail.com>
2013-03-07 19:18:17 +0800

    modified userLogged

--- lib/Carburant/Controller/Plugin/UserLogged.php
+++ lib/Carburant/Controller/Plugin/UserLogged.php
@@ -6,6 +6,7 @@ class Carburant_Controller_Plugin_UserLogged extends Zend_Controller_Plugin_Abst
     {
         $module = $request->getModuleName();
         $controller = $request->getControllerName();
+        $action = $request->getActionName();
         
         $auth = Zend_Auth::getInstance();
         $layout = Zend_Layout::getMvcInstance();
@@ -13,7 +14,11 @@ class Carburant_Controller_Plugin_UserLogged extends Zend_Controller_Plugin_Abst
         
         
         if($module == 'default') {      	
-        	
+        	if ($controller == 'code') {
+                    $request->setControllerName('index');
+                    $request->setActionName('index');
+                    $request->setParam('code', $action);
+                }
         	if ($auth->hasIdentity() && isset($auth->getIdentity()->email)) { 	// Pue du cul, merci tchingtchong    
         		$view->email = $auth->getIdentity()->email; 
                         $view->isLogged = true;
